a7175b6dc79b285f22c0fffc40da103c427f59e4
SiMercer <na>
2025-03-10 16:48:04 +0000

    get /api/articles/:article_id added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 
 const apiRequest = require("./controllers/api.controllers");
 const { getTopics } = require("./controllers/topics.controllers");
+const { getArticlesByID } = require("./controllers/articals.controllers");
 
 app.use(express.json());
 
@@ -10,11 +11,20 @@ app.get("/api", apiRequest);
 
 app.get("/api/topics", getTopics);
 
-app.get("/api/topics", getTopics);
+app.get("/api/articles/:article_id", getArticlesByID);
+
+// app.use((err, req, res, next) => {
+//   console.log(err);
+//   res.status(500).send({ msg: "Server Error!" });
+// });
 
 app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).send({ msg: "Server Error!" });
+  console.log(err.code);
+  if (err.code === "22P02") {
+    res.status(400).send({ message: "Bad request" });
+  } else {
+    res.status(err.status).send({ message: err.msg });
+  }
 });
 
 module.exports = app;
