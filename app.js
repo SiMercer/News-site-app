f87e23506555a3d5713f0e84fa19cdb8d3ac1ec4
SiMercer <na>
2025-03-31 10:22:56 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const apiRequest = require("./controllers/api.controllers");
@@ -7,15 +8,22 @@ const {
   getArticles,
   getArticleByID,
   patchArticleByID,
+  postArticle,
 } = require("./controllers/articles.controllers");
 
 const {
   postCommentsByArticleByID,
   getCommentsByArticleByID,
   deleteCommentByID,
+  patchCommentVotes,
 } = require("./controllers/comments.controllers");
 
-const { getUsers } = require("./controllers/users.controllers");
+const {
+  getUsers,
+  getUserByUsername,
+} = require("./controllers/users.controllers");
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -25,6 +33,8 @@ app.get("/api/topics", getTopics);
 
 app.get("/api/articles", getArticles);
 
+app.post("/api/articles", postArticle);
+
 app.get("/api/articles/:article_id", getArticleByID);
 
 app.patch("/api/articles/:article_id", patchArticleByID);
@@ -33,9 +43,13 @@ app.get("/api/articles/:article_id/comments", getCommentsByArticleByID);
 
 app.post("/api/articles/:article_id/comments", postCommentsByArticleByID);
 
+app.patch("/api/comments/:comment_id", patchCommentVotes);
+
 app.delete("/api/comments/:comment_id", deleteCommentByID);
 
 app.get("/api/users", getUsers);
+
+app.get("/api/users/:username", getUserByUsername);
 
 app.all("*", (request, response, next) => {
   response.status(404).send({ msg: "path not found" });
